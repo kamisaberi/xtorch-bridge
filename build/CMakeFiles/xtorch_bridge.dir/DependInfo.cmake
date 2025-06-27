848749fa37ad2ce6c18ed4abe0c9c2400af8407f
kamisaberi <kamisaberi@gmail.com>
2025-06-27 18:05:06 +0330

    some files reordered

---
+++ build/CMakeFiles/xtorch_bridge.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kami/Documents/cpp/xtorch-bridge/src/bindings.cpp" "CMakeFiles/xtorch_bridge.dir/src/bindings.cpp.o" "gcc" "CMakeFiles/xtorch_bridge.dir/src/bindings.cpp.o.d"
+  "/home/kami/Documents/cpp/xtorch-bridge/src/python_data_loader_bridge.cpp" "CMakeFiles/xtorch_bridge.dir/src/python_data_loader_bridge.cpp.o" "gcc" "CMakeFiles/xtorch_bridge.dir/src/python_data_loader_bridge.cpp.o.d"
+  "" "xtorch_bridge.cpython-37m-x86_64-linux-gnu.so" "gcc" "CMakeFiles/xtorch_bridge.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/xtorch_bridge.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xtorch_bridge.dir/link.d"
+  "CMakeFiles/xtorch_bridge.dir/src/bindings.cpp.o"
+  "CMakeFiles/xtorch_bridge.dir/src/bindings.cpp.o.d"
+  "CMakeFiles/xtorch_bridge.dir/src/python_data_loader_bridge.cpp.o"
+  "CMakeFiles/xtorch_bridge.dir/src/python_data_loader_bridge.cpp.o.d"
+  "xtorch_bridge.cpython-37m-x86_64-linux-gnu.so"
+  "xtorch_bridge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xtorch_bridge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
